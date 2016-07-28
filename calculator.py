ec3b6b953aae4a5b9050c3506b28d80727fb38a1
Your Name <you@example.com>
2016-07-27 20:54:55 -0700

    calc

---
+++ calculator.py
@@ -0,0 +1,37 @@
+def add (num1, num2):
+	return num1 + num2
+
+
+def subtract(num1, num2):
+	return num1-num2
+
+
+def multiply (num1, num2):
+	return num1 * num2
+
+
+def divide(num1, num2):
+	return num1/num2
+
+# print divide(100,10) 
+
+def modulo (num1, num2):
+	return num1 % num2
+# print modulo (5, 4)
+
+def power(base,exponent):
+	return base ** exponent
+
+# print power(2,3) 
+
+def square (num):
+	return num **2
+# print square (4)
+
+
+x = modulo(3,4)
+y = divide(x,9)
+
+print x
+print y
+print multiply(y,9)
